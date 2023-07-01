82da4343f42ca8a046af0411b6f542860b8051c9
jhbricks <93678356+jhbricks@users.noreply.github.com>
2023-06-30 21:17:24 -0300

    Update 4_Teste.py

--- pages/4_Teste.py
+++ pages/4_Teste.py
@@ -26,3 +26,42 @@ st.markdown(
     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )
+
+
+import streamlit as st
+
+m = st.markdown("""
+<style>
+div.stButton > button:first-child { 
+    background-color: #ce1126;
+    color: white;
+    height: 3em;
+    width: 12em;
+    border-radius:10px;
+    border:3px solid #000000;
+    font-size:20px;
+    font-weight: bold;
+    margin: auto;
+    display: block;
+}
+
+div.stButton > button:hover {
+	background:linear-gradient(to bottom, #ce1126 5%, #ff5a5a 100%);
+	background-color:#ce1126;
+}
+
+div.stButton > button:active {
+	position:relative;
+	top:3px;
+}
+
+</style>""", unsafe_allow_html=True)
+
+b = st.button("Button 1")
+
+
+st.markdown('<p></p>', unsafe_allow_html = True)
+st.markdown('<p></p>', unsafe_allow_html = True)
+st.markdown('<p></p>', unsafe_allow_html = True)
+
+c = st .button("Button 2")
